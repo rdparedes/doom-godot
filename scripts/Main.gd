4157f78b0b3637e4a1b480451e9b14880628cd5e
Roberto Paredes <rdparedessalazar@gmail.com>
2018-12-17 14:52:25 -0500

    show/hide HUD on keypress

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,7 +6,7 @@ func _ready():
   $Projection.player.position = $StartPosition.position
   PROJECTION_TO_360_RATIO = $Projection.ANGLE360 / 360
   var map_representation = []
-  for n in $Obstacles.get_children():
+  for n in $HUD/Obstacles.get_children():
     map_representation.append(
       [
         int(n.position.x / $Projection.grid_unit_size),
@@ -20,8 +20,12 @@ func _physics_process(delta):
 
 func _process(delta):
   $Projection.update()
-  $Player.position = $Projection.player.position
-  $Player.rotation_degrees = floor($Projection.player.rotation / PROJECTION_TO_360_RATIO)
-  $Debug.update_player_angle($Player.rotation_degrees)
+  $HUD/Player.position = $Projection.player.position
+  $HUD/Player.rotation_degrees = floor($Projection.player.rotation / PROJECTION_TO_360_RATIO)
+
+  if Input.is_action_just_pressed('ui_show_hide_hud'):
+    $HUD.show() if $HUD.visible == false else $HUD.hide()
+
+  $Debug.update_player_angle($HUD/Player.rotation_degrees)
   $Debug.update_player_position($Projection.player.position)
   $Debug.update_ray($Projection.debug_first_ray, $Projection.debug_last_ray)
